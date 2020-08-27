123b320e011d9035270ede02103bb49d7562c077
Mustafa Demirpençe <m.demirpence06@gmail.com>
2020-08-27 14:15:41 +0300

    Land Mark App
    Kent Simgeleri Uyg.

---
+++ LandMarkBook/app/src/main/java/com/mdemirpence/landmarkbook/MainActivity.java
@@ -0,0 +1,87 @@
+package com.mdemirpence.landmarkbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        //data
+
+        final ArrayList <String> landmarkNames = new ArrayList<>();
+        landmarkNames.add("Pisa");
+        landmarkNames.add("Eiffrel");
+        landmarkNames.add("Empire State");
+        landmarkNames.add("Galata");
+
+
+        final ArrayList<String> countryNames = new ArrayList<>();
+        countryNames.add("Italy");
+        countryNames.add("France");
+        countryNames.add("USA");
+        countryNames.add("Turkey");
+
+        Bitmap pisa = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.pisa);
+        Bitmap eiffel = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.eiffel);
+        Bitmap empirestate = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.empirestate);
+        Bitmap galata = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.galata);
+
+        final ArrayList<Bitmap> landmarkImages = new ArrayList<>();
+
+        landmarkImages.add(pisa);
+        landmarkImages.add(eiffel);
+        landmarkImages.add(empirestate);
+        landmarkImages.add(galata);
+
+
+
+
+        //listview
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,landmarkNames);
+
+        listView.setAdapter(arrayAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //System.out.println(landmarkNames.get(i));
+                //System.out.println(countryNames.get(i));
+
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                intent.putExtra("name", landmarkNames.get(i));
+                intent.putExtra("country" , countryNames.get(i));
+
+                Singleton singleton = Singleton.getInstance();
+                singleton.setChosenImage(landmarkImages.get(i));
+
+
+
+
+
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+}
